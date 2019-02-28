93e7f2a348cbace35a2840225b51344f2b492ef7
vijay-dvg <vijay.dvg88@gmail.com>
2019-02-27 19:24:10 -0600

    Adding "spring-boot-hystrix-dashboard" repo

---
+++ src/main/java/com/vijay/spring/boot/hystrix/dashboard/SpringBootHystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.vijay.spring.boot.hystrix.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHystrixDashboardApplication.class, args);
+	}
+
+}
